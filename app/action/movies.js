89c953578036073a3d4b9139cdce4295ab179788
bernardstanislas <bernardstanislas@gmail.com>
2016-01-29 14:08:36 +0100

    Add store listener

--- app/action/movies.js
+++ app/action/movies.js
@@ -1,4 +1,13 @@
-import {loadMovie} from '../services/movies'
-export function loadMovieAction(criteria){
-    return loadMovie(criteria).then(d => console.log(d));
+import {loadMovie} from '../services/movies';
+import dispatcher from 'focus-core/dispatcher';
+
+export function loadMovieAction(criteria) {
+    return loadMovie(criteria).then(movie => {
+        dispatcher.handleServerAction({
+            data: {
+                movieInfo: movie
+            },
+            type: 'update'
+        });
+    });
 }
--- app/views/movie/informations.js
+++ app/views/movie/informations.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Panel from 'focus-components/components/panel';
 import {mixin as formPreset} from 'focus-components/common/form';
 import {loadMovieAction} from '../../action/movies';
+import movieStore from '../../stores/movies';
 export default React.createClass({
-  mixins: [formPreset],
-  definitionPath: 'movie',
-  action: {
-    load: loadMovieAction
-  },
-  renderContent(){
-    const {id} = this.props;
-    return (
-      <Panel title='Mon superbe block'>
-        Mon block 1 {id}
-      </Panel>
-    );
-  }
+    mixins: [formPreset],
+    definitionPath: 'movie',
+    action: {
+        load: loadMovieAction
+    },
+    stores: [{
+        store: movieStore,
+        properties: ['movieInfo']
+    }],
+    renderContent() {
+        const {id} = this.props;
+        const {title} = this.state;
+        return (
+            <Panel title='Informations'>
+                <h3>{title}</h3>
+            </Panel>
+        );
+    }
 })
